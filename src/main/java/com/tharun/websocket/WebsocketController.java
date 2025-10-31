b23d0287f9c16914b085b64bdfe52d6c98fef734
Tharun R <Tharuntechie@gmail.com>
2025-11-01 02:10:52 +0530

    Regular Check Up!!!

--- src/main/java/com/tharun/websocket/WebsocketController.java
+++ src/main/java/com/tharun/websocket/WebsocketController.java
@@ -1,4 +1,27 @@
 package com.tharun.websocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class WebsocketController {
+    private final SimpMessagingTemplate messagingTemplate;
+
+
+    @Autowired
+    public WebsocketController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+
+    }
+
+
+    @MessageMapping("/message")
+    public void handelMessage(Message message) {
+        System.out.println("Received from user: " + message.getUser() + ":" + message.getMessage());
+        messagingTemplate.convertAndSend("/topic/message", message);
+        System.out.println("Sent message to /topic/message: " + message.getUser()+ ": "+message.getMessage());
+    }
 }
